267c2b53d3641d212c4487891d4a2bf5f4d48271
lansanasylla <lansana.sylla@gn.ehealth.org>
2016-10-17 00:34:27 +0000

    delete backup directory

--- backup/inc/footer.inc.php
+++
@@ -1,6 +0,0 @@
-  <!--footer-->
-<footer class="footer">
-        <div class="container">
-        <p class="text-muted">By <a href="http://www.ehealthafrica.org/">eHealthAfrica 2016</a></p>
-        </div>
-</footer>
